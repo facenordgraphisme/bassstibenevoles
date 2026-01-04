c9009f54640c850c27d871bcf675d4155b0c22b0
Fx <francoisxavierpin@gmail.com>
2026-01-04 10:56:40 +0100

    good

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,13 +2,6 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-
-if (!projectId) {
-  console.error("❌ Project ID is missing!");
-  console.error("Ensure .env.local exists in root and contains NEXT_PUBLIC_SANITY_PROJECT_ID");
-} else {
-  console.log("✅ Project ID loaded:", projectId);
-}
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'sanity-project-id';
 
 export const useCdn = false
